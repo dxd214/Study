b309b03b87f6d04ed22b816545f606c875476686
JiangDi <JiangDi@JiangDi.local>
2016-05-12 23:53:51 +0800

    更新

---
+++ Day31_新知识_Swift入门/01_第一个Swift程序.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let redView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+redView.backgroundColor = UIColor.redColor()
+
+let button = UIButton(type: UIButtonType.ContactAdd);
+redView.addSubview(button);
---
+++ Day31_新知识_Swift入门/02_常量和变量.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// let为常量，var为变量：
+// 按住option键点击鼠标查看数据类型：
+let num1 = 10
+let num2 = 3.14
+
+// 变量可以改值，但是只能是类型相同的值：
+var num3 = 10
+//num = 3.14
+num3 = 13
+
+var num4 = 1.2
+num4 = 3.14
+
+// 打印：
+print(num1, num2, num3, num4)
